39a32eb876aaafb4724b5ebfc61624f8dbbba4c5
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-04 23:15:49 +0300

    1.Created Mark class 2.Created a vector with marks in Lab3 class 3.Added 7 marks in the vector

--- pip/lab3/src/com/bildeyko/Lab3.java
+++ pip/lab3/src/com/bildeyko/Lab3.java
@@ -1,8 +1,20 @@
 package com.bildeyko;
 
+import java.util.Vector;
+
 public class Lab3 {
 
     public static void main(String[] args) {
-	// write your code here
+        Vector<Mark> marks = new Vector<Mark>();
+        marks.add(new Mark(4,-3));
+        marks.add(new Mark(-2,1));
+        marks.add(new Mark(-4,-4));
+        marks.add(new Mark(-5,4));
+        marks.add(new Mark(2,1));
+        marks.add(new Mark(0,0));
+        marks.add(new Mark(5,4));
+        marks.add(new Mark(2,-1));
+
+        System.out.print("Hello!");
     }
 }

06b1c5f61249e7371420b6df6b5854bda5eea0aa
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-05 01:04:30 +0300

    1. Add chechMark method in Figure class 2. Complete cycle in Lab3 class, which checks marks in collection

--- pip/lab3/src/com/bildeyko/Lab3.java
+++ pip/lab3/src/com/bildeyko/Lab3.java
@@ -15,6 +15,15 @@ public class Lab3 {
         marks.add(new Mark(5,4));
         marks.add(new Mark(2,-1));
 
+        Figure newFigure = new Figure(12);
+        int i=0;
+        System.out.print("List of marks which located inside of region:\n");
+        do {
+            Mark current = marks.get(i);
+            if(newFigure.checkMark(current))
+                System.out.printf("Mark (%.2f,%.2f) - %n", current.x, current.y);
+            i++;
+        } while (i < marks.size());
         System.out.print("Hello!");
     }
 }

84869a80cec72344b59313ba6f2b83d5ecf2abf7
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-07 21:30:42 +0300

    Implemented the infinitive loop with class which implements Iterable<T>

---
+++ pip/Inf_iterator_7.11/src/com/company/InfIterator.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Iterator;
+
+/**
+ * Created by ASUS on 07.11.2014.
+ */
+public class InfIterator<T> implements Iterator<T> {
+    public boolean hasNext() {
+        return true;
+    }
+
+    public T next() {
+        return null;
+    }
+
+    public void remove() {
+
+    }
+
+}
---
+++ pip/Inf_iterator_7.11/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    InfClass<Integer> temp = new InfClass<Integer>();
+
+        for(Integer o: temp) {
+            System.out.print("Hello world!\n");
+        }
+    }
+}

485089d384ba11de19464df5dfa927b7a53fc89b
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-17 21:26:03 +0300

    Create copy of lab3

---
+++ pip/lab4/src/com/bildeyko/Mark.java
@@ -0,0 +1,14 @@
+package com.bildeyko;
+
+/**
+ * Created by ASUS on 04.11.2014.
+ */
+public class Mark {
+    public float x;
+    public float y;
+
+    public Mark(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}

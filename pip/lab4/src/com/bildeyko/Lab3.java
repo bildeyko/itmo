55017c8f2e0b451987be32432812ca43729ba5f5
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-23 18:12:05 +0300

    Небольшие изменения

--- pip/lab4/src/com/bildeyko/Lab3.java
+++ pip/lab4/src/com/bildeyko/Lab3.java
@@ -3,9 +3,21 @@ package com.bildeyko;
 import java.util.Iterator;
 import java.util.Vector;
 
+import javax.swing.*;
+
+import com.bildeyko.views.MainView;
+
 public class Lab3 {
 
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainView();
+            }
+        });
+
         if(args.length != 0) {
             double r = 8;
             try {

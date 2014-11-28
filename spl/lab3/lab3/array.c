8f4b1d9055ebb79fd549b3ceb54dfe52044920f8
Nickolay Bildeyko <bildeyko@gmail.com>
2014-11-28 09:54:56 +0300

    Add feof()

--- spl/lab3/lab3/array.c
+++ spl/lab3/lab3/array.c
@@ -19,16 +19,30 @@ array_t * array_create(size_t size)
 
 array_t * array_get(FILE * file)
 {
+	/*
+		Способы вывода ошибок:
+		1. Глобальная переменная
+		2. Возвращать некий err_t - err_t array_get(array_t ** arr, FILE * file)
+		*/
 	size_t size;
 	int num, i, *buf;
 	array_t * arr, temp;
 	fscanf(file, "%d", &size);
+	if (feof(file)) {
+		showError("Errors in a structure of the file.");
+		return NULL;
+	}
 	arr = array_create(size);
 	if (arr == NULL)
 		return NULL;
 	for (i = 0; i < arr->size; i++) 
 	{
+		if (feof(file)) {
+			showError("Errors in a structure of the file.");
+			return NULL;
+		}
 		fscanf(file, "%d", &num);
+		
 		arr->arrayPtr[i] = num;
 	}
 	return arr;

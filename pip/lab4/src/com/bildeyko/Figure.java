3105773e3ea397bf30ceb4f16e6d503b401e6642
Nickolay Bildeyko <bildeyko@gmail.com>
2014-12-05 12:01:40 +0300

    1234

--- pip/lab4/src/com/bildeyko/Figure.java
+++ pip/lab4/src/com/bildeyko/Figure.java
@@ -12,15 +12,15 @@ public class Figure {
 
     public boolean checkMark(Mark mark) {
         if(mark.x >= 0) {
-            if(mark.y >= 0 && mark.y <= (r/2 - mark.x)) {
+            if(mark.y >= 0 && mark.y <= r && mark.x <= r) {
                 return true;
             }
-            if(mark.y < 0 && mark.y >= (-r/2) && mark.x <= r) {
+            if(mark.y < 0 && mark.y >= (mark.x-r)/2) {
                 return true;
             }
         } else {
             double radius = Math.sqrt(Math.pow(mark.x,2)+Math.pow(mark.y,2));
-            if(mark.y <= 0 && radius <= r/2) {
+            if(mark.y >= 0 && radius <= r) {
                 return true;
             }
         }
--- pip/lab4/src/com/bildeyko/views/MainView.java
+++ pip/lab4/src/com/bildeyko/views/MainView.java
@@ -1,20 +1,88 @@
 package com.bildeyko.views;
 
+import com.bildeyko.Figure;
+import com.bildeyko.views.tools.PlaneCanvas;
+import com.bildeyko.views.tools.Plane;
+
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 /**
  * Created by ASUS on 18.11.2014.
  */
 public class MainView {
+    final Plane plane;
+    Figure newFigure;
     public MainView() {
         JFrame frm = new JFrame("Область");
-        //frm.setLayout(new BoxLayout(frm.getContentPane(), BoxLayout.Y_AXIS));
+        frm.setLayout(new BoxLayout(frm.getContentPane(), BoxLayout.Y_AXIS));
 
         frm.setSize(500,500);
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new GridLayout(0,2));
+        controlPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+        frm.add(controlPanel);
+
+        JPanel listXCont = new JPanel();
+        listXCont.setLayout(new FlowLayout());
+        listXCont.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+        //listXCont.setMaximumSize(new Dimension(200, 100));
+        String[] itemsXCoor = {"2", "5", "10"};
+        JList listX = new JList(itemsXCoor);
+        listX.setVisibleRowCount(-1);
+        listXCont.add(listX);
+        controlPanel.add(new JLabel("X"));
+        controlPanel.add(listXCont);
+       // frm.add(controlPanel);
+        frm.add(controlPanel, BorderLayout.NORTH);
+
+        JPanel yButPanel = new JPanel();
+        yButPanel.setLayout(new BoxLayout(yButPanel, BoxLayout.Y_AXIS));
+        ButtonGroup yGroup = new ButtonGroup();
+        JRadioButton yCoor1 = new JRadioButton("1", true);
+        JRadioButton yCoor3 = new JRadioButton("3");
+        JRadioButton yCoor15 = new JRadioButton("15");
+        yGroup.add(yCoor1);
+        yGroup.add(yCoor3);
+        yGroup.add(yCoor15);
+        yButPanel.add(yCoor1);
+        yButPanel.add(yCoor3);
+        yButPanel.add(yCoor15);
+        controlPanel.add(new JLabel("Y"));
+        controlPanel.add(yButPanel);
+
+        JPanel rPanel = new JPanel();
+        rPanel.setLayout(new FlowLayout());
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(5,1,20,1));
+        rPanel.add(spinner);
+        controlPanel.add(new JLabel("R"));
+        controlPanel.add(rPanel);
+        ChangeListener listener = new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                JSpinner spinnerTemp = (JSpinner)e.getSource();
+                SpinnerModel spinnerModel = spinnerTemp.getModel();
+                System.out.println("Source: " + spinnerModel.getValue());
+                plane.setRadius((Integer)spinnerModel.getValue());
+                newFigure = new Figure((Integer)spinnerModel.getValue());
+            }
+        };
+
+        spinner.addChangeListener(listener);
+
+       plane = new Plane(200,200);
+        plane.setPoint(10,10);
+        /*JPanel planePanel = new JPanel();
+        planePanel.setLayout(new FlowLayout());
+        PlaneCanvas test = new PlaneCanvas();
+        planePanel.add(test);
+        test.setSize(200,200);*/
+        frm.add(plane);
+
+        /*JPanel controlPanel = new JPanel();
         controlPanel.setLayout(new GridLayout(10,1));
         controlPanel.setVisible(true);
         controlPanel.setSize(100,100);
@@ -27,7 +95,6 @@ public class MainView {
         xPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
         String[] itemsXCoor = {"2", "5", "10"};
         JComboBox xBox = new JComboBox(itemsXCoor);
-        //xBox.setMaximumSize(new Dimension(200, 100));
         xPanel.add(new JLabel("X"));
         xPanel.add(Box.createRigidArea(new Dimension(5,0)));
         xPanel.add(xBox);
@@ -52,12 +119,13 @@ public class MainView {
         yPanel.add(yButPanel);
 
         controlPanel.add(xPanel);
-        controlPanel.add(yPanel);
+        controlPanel.add(yPanel);*/
 
         //JLabel lab = new JLabel("Hello!");
         //frm.add(lab);
 
        // frm.setContentPane(inputPanel);
+        frm.pack();
         frm.setVisible(true);
     }
 }

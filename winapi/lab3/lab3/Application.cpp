ec67a1fef1c5816383edeff756e02c773a96a5f7
Nikolay Bildeyko <bildeyko@gmail.com>
2015-10-12 01:48:01 +0300

    api lab 3

---
+++ winapi/lab3/lab3/Application.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <tchar.h>
+#include "Application.h"
+#include "MyWindow.h"
+using namespace std;
+
+Application::Application(HINSTANCE hInstance) {
+	hInst = hInstance;
+
+	MyWindow window1(hInst, "Window 1", 500, 500);
+	window1.ShowWindow();
+	window1.UpdateWindow();
+
+	MyWindow window2(hInst, "Window 2", 300, 100);
+	window2.ShowWindow();
+	window2.UpdateWindow();
+}
+
+int Application::Run() {
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
